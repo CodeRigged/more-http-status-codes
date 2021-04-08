fb49426f160ad73fa4993c9a8737938efeaa877e
Timothy Antonovics <tantonovics@trilobyte-se.de>
2021-04-08 19:29:15 +0200

    initalizes index and adds function to check for supported status codes by node

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,73 @@
-const messages: string[] = ['Happy', 'Coding!'];
+import InformationalCodes from './enums/InformationalCodes';
+import SuccessfulCodes from './enums/SuccessfulCodes';
+import RedirectionCodes from './enums/RedirectionCodes';
+import ClientErrorCodes from './enums/ClientErrorCodes';
+import ServerErrorCodes from './enums/ServerErrorCodes';
+import WebDAVCodes from './enums/WebDAVCodes';
+import { STATUS_CODES } from 'http';
 
-console.log(
-    messages.reduce((message, curVal) => {
-        return `${message}${curVal} `;
-    }, ''),
-);
+/**
+ * @type documented HTTP response status codes
+ */
+type OfficialHttpStatusCodes = typeof InformationalCodes &
+    typeof SuccessfulCodes &
+    typeof RedirectionCodes &
+    typeof ClientErrorCodes &
+    typeof ServerErrorCodes &
+    typeof WebDAVCodes;
+
+/**
+ * @description Object which contains all official HTTP response status codes.
+ *
+ * @readonly Object is immutable
+ *
+ * {@link https://github.com/CodeRigged/more-http-status-codes Further reading...}
+ */
+const HttpStatusCodes: OfficialHttpStatusCodes = {
+    ...WebDAVCodes,
+    ...InformationalCodes,
+    ...SuccessfulCodes,
+    ...RedirectionCodes,
+    ...ClientErrorCodes,
+    ...ServerErrorCodes,
+} as const;
+
+/** Keys of supported HTTP response status codes by node */
+const supportedByNodeStatusCodes = Object.keys(STATUS_CODES);
+
+/** Keys of documented HTTP response status codes by this module */
+const httpStatusCodes = Object.keys(HttpStatusCodes) as (keyof typeof HttpStatusCodes)[];
+
+/**
+ * @description Function which checks {@link https://nodejs.org/api/http.html#http_http_status_codes nodes}
+ * collection of all the standard HTTP response status codes and compares it to the ones defined
+ * in this module.
+ *
+ * @param supported if false, this function will return all unsupported HTTP response status codes
+ * @returns array of strings
+ */
+const listSupportedCodesByNode = (supported = true): string[] => {
+    const codes = httpStatusCodes
+        .filter((key) => isNaN(HttpStatusCodes[key]))
+        .reduce(
+            (acc, key) => {
+                if (supportedByNodeStatusCodes.includes(key)) {
+                    acc.supported.push(key);
+                } else {
+                    acc.unsupported.push(key);
+                }
+                return acc;
+            },
+            { supported: [] as string[], unsupported: [] as string[] },
+        );
+    return supported ? codes.supported : codes.unsupported;
+};
+
+// utility functions
+export { listSupportedCodesByNode };
+
+// categorized HTTP response status codes
+export { InformationalCodes, WebDAVCodes, SuccessfulCodes, RedirectionCodes, ClientErrorCodes, ServerErrorCodes };
+
+// default object contains all status codes
+export default HttpStatusCodes;
